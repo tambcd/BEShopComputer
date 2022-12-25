6119376220467fd04317a987b722990f4bfac73c
TRAN VAN TAM <tam191203380@st.utc.edu.vn>
2022-12-25 14:04:43 +0700

    fist

---
+++ ShopComputerTam/Common/Models/Entity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopComputerTam.Common.Models
+{
+    public class Entity
+    {
+        /// <summary>
+        /// Ngày tạo 
+        /// </summary>
+        public DateTime? CreateDate { get; set; }
+        /// <summary>
+        /// người tạo 
+        /// </summary>
+        public string? CreateBy { get; set; }
+        /// <summary>
+        /// ngày thay đổi 
+        /// </summary>
+        public DateTime? ModifiedDate { get; set; }
+        /// <summary>
+        /// người thay đổi
+        /// </summary>
+
+        public string? ModifiedBy { get; set; }
+    }
+}
